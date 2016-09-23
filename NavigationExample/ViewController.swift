556767214d23dc249aef5824051f63aba608c792
Infraestructura <Infraestructura@imac-de-alumno-7.local>
2016-09-23 13:13:43 -0500

    Initial Commit

---
+++ NavigationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationExample
+//
+//  Created by Infraestructura on 23/09/16.
+//  Copyright © 2016 Nitrogena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
